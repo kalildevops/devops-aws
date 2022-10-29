1d24912b426d665eec3e2d105e22375cfed65c37
kalil_mori <kalil.devops@gmail.com>
2022-10-29 19:34:10 -0300

    fix tag

--- iac/terraform/modules/sns/main.tf
+++ iac/terraform/modules/sns/main.tf
@@ -1,4 +1,4 @@
 resource "aws_sns_topic" "this" {
   name = "${var.project}-sns-topic-${var.env}"
-  merge({ Name = "${var.project}-sns-topic-${var.env}" }, var.tags)
+  tags = merge({ Name = "${var.project}-sns-topic-${var.env}" }, var.tags)
 }
