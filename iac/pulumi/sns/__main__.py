1490d6f46227c8a693c0cff1a91a6a648a3c1ae7
Kalil Mori <kalil.devops@gmail.com>
2022-11-12 17:05:28 -0300

    update SNS name

--- iac/pulumi/sns/__main__.py
+++ iac/pulumi/sns/__main__.py
@@ -2,5 +2,7 @@ import pulumi
 from pulumi_aws import sns
 
 # Create SNS topic
-sns_topic = sns.Topic("topic-test-pulumi")
+sns_topic = sns.Topic('topic-test-pulumi', {
+    name='topic-test-pulumi'
+})
 
